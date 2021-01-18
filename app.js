d8c3074d5c3148e69c1c8f01a49946d83a8731f4
Adnan Rahic <adnan.rahic@sematext.com>
2021-01-18 17:05:54 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const httpLogger = require('./httpLogger')
+
+app.get('/bam', (req, res, next) => {
+  console.log('Bam!')
+  res.status(200).send('Bam!')
+})
+
+app.use(httpLogger)
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Hello World!')
+})
+
+app.get('/boom', (req, res, next) => {
+  try {
+    throw new Error('Wowza!')
+  } catch (error) {
+    logger.error('Whooops! This broke with error: ', error)
+    res.status(500).send('Error!')
+  }
+})
+
+app.get('/errorhandler', (req, res, next) => {
+  try {
+    throw new Error('Wowza!')
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use(logErrors)
+app.use(errorHandler)
+
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+function errorHandler (err, req, res, next) {
+  res.status(500).send('Error!')
+}
+
+const port = process.env.PORT || 80
+app.listen(port, () =>
+  logger.info(`Express.js listening on port ${port}.`))
